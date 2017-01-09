619e750455d4d4beee7f45fab944ae6cf7a33740
Alice Morris <alicemorris@oca.ac.uk>
2017-01-09 15:29:53 +0000

    Tertiary Commit

--- wp-content/themes/newtheme/header.php
+++ wp-content/themes/newtheme/header.php
@@ -8,7 +8,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title><?php echo get_bloginfo( 'name' ); ?></title>
+	<title>Blog Template for Bootstrap</title>
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
 	<link href="<?php bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -25,7 +25,7 @@
 	   <div class="container">
 		  <nav class="blog-nav">
 			<a class="blog-nav-item active" href="#">Home</a>
-			<?php wp_list_pages( '&title_li=' ); ?>
+			 <?php wp_list_pages( '&title_li=' ); ?>
 		  </nav>
 	   </div>
     </div>
