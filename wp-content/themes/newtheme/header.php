a9775c096b2677837bbd4e3788247205feb1d8ca
Alice Morris <alicemorris@oca.ac.uk>
2017-01-10 16:51:41 +0000

    Part 1 Complete

--- wp-content/themes/newtheme/header.php
+++ wp-content/themes/newtheme/header.php
@@ -8,31 +8,28 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-	<title>Blog Template for Bootstrap</title>
-	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
-	<link href="<?php bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+	<title>
+		<?php wp_title( '|', true, 'right' ); ?>
+	</title>
+
 	<?php wp_head();?>
 </head>
 
 <body>
 
-    <div class="blog-masthead">
-	   <div class="container">
-		  <nav class="blog-nav">
-			<a class="blog-nav-item active" href="#">Home</a>
-			 <?php wp_list_pages( '&title_li=' ); ?>
-		  </nav>
-	   </div>
-    </div>
-	
+	<div class="blog-masthead">
+		<div class="container">
+			<nav class="blog-nav">
+				<?php wp_list_pages( '&title_li='); ?>
+			</nav>
+		</div>
+	</div>
+
 	<div class="container">
 
 		<div class="blog-header">
-			<h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-			<p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
+			<h1 class="blog-title"><a href="<?php bloginfo('wpurl');?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+			<p class="lead blog-description">
+				<?php echo get_bloginfo( 'description' ); ?>
+			</p>
 		</div>
